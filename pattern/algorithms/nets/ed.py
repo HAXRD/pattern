63173601eb025c2a4d064bc181db5aa26a2d7d7e
HAXRD@lyus <haxrd@stu.xmu.edu.cn>
2021-10-10 22:34:35 +0800

    resolve not eval problem

--- pattern/algorithms/nets/ed.py
+++ pattern/algorithms/nets/ed.py
@@ -12,13 +12,13 @@ class EncoderDecoder(nn.Module):
         self.n_out_chs = n_out_chs
 
         # Encoder
-        self.conv1 = nn.Conv2d(n_in_chs, 64, 3, padding=1)
-        self.conv2 = nn.Conv2d(64, 32, 3, padding=1)
+        self.conv1 = nn.Conv2d(n_in_chs, 128, 3, padding=1)
+        self.conv2 = nn.Conv2d(128, 64, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
 
         # Decoder
-        self.t_conv1 = nn.ConvTranspose2d(32, 64, 2, stride=2)
-        self.t_conv2 = nn.ConvTranspose2d(64, n_out_chs, 2, stride=2)
+        self.t_conv1 = nn.ConvTranspose2d(64, 128, 2, stride=2)
+        self.t_conv2 = nn.ConvTranspose2d(128, n_out_chs, 2, stride=2)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
--- pattern/train_sse_normal.sh
+++ pattern/train_sse_normal.sh
@@ -11,6 +11,7 @@ n_GU=80
 R_2D_NLoS=100
 R_2D_LoS=200
 episode_len=40.
+num_base_env_episodes=100000
 emulator_replay_size=1000000
 policy_replay_size=100000
 num_planning_random=8192
@@ -31,6 +32,6 @@ emulator_batch_size=128
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=0 python train_sse.py --experiment_name ${exp} --seed ${seed} --base_BMs_fname ${base_bm} --BMs_fname ${bm} --world_len ${world_len} --granularity ${granularity} --episode_length ${episode_len} --n_ABS ${n_ABS} --n_GU ${n_GU} --R_2D_NLoS ${R_2D_NLoS} --R_2D_LoS ${R_2D_LoS} --emulator_replay_size ${emulator_replay_size} --policy_replay_size ${policy_replay_size} --num_planning_random_warmup ${num_planning_random_warmup} --num_planning_random ${num_planning_random} --num_planning_with_policy ${num_planning_with_policy} --planning_top_k ${top_k} --planning_batch_size ${planning_batch_size} --num_base_emulator_episodes ${num_base_emulator_episodes} --num_base_emulator_epochs ${num_base_emulator_epochs} --num_base_emulator_batch_size ${num_base_emulator_batch_size} --num_env_episodes ${num_env_episodes} --least_emulator_buffer_size ${least_emulator_buffer_size} --least_policy_buffer_size ${least_policy_buffer_size} --num_train_policy ${num_train_policy} --policy_batch_size ${policy_batch_size} --num_train_emulator ${num_train_emulator} --emulator_batch_size ${emulator_batch_size} --use_emulator_pt
+    CUDA_VISIBLE_DEVICES=0 python train_sse.py --experiment_name ${exp} --seed ${seed} --base_BMs_fname ${base_bm} --BMs_fname ${bm} --world_len ${world_len} --granularity ${granularity} --episode_length ${episode_len} --n_ABS ${n_ABS} --n_GU ${n_GU} --R_2D_NLoS ${R_2D_NLoS} --R_2D_LoS ${R_2D_LoS} --emulator_replay_size ${emulator_replay_size} --policy_replay_size ${policy_replay_size} --num_planning_random_warmup ${num_planning_random_warmup} --num_planning_random ${num_planning_random} --num_planning_with_policy ${num_planning_with_policy} --planning_top_k ${top_k} --planning_batch_size ${planning_batch_size} --num_base_emulator_episodes ${num_base_emulator_episodes} --num_base_emulator_epochs ${num_base_emulator_epochs} --num_base_emulator_batch_size ${num_base_emulator_batch_size} --num_env_episodes ${num_env_episodes} --least_emulator_buffer_size ${least_emulator_buffer_size} --least_policy_buffer_size ${least_policy_buffer_size} --num_train_policy ${num_train_policy} --policy_batch_size ${policy_batch_size} --num_train_emulator ${num_train_emulator} --emulator_batch_size ${emulator_batch_size} --num_base_env_episodes ${num_base_env_episodes} --use_emulator_pt --use_eval
 done
 # --use_emulator_pt
